b2ccf05e5db7aa01f7c6111b80e7778659912c37
supun-pregeeth <ruchiraniasanka@gmail.com>
2026-02-04 00:16:42 +0530

    add controller part

--- auth-service/src/main/java/com/ruhuna/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/ruhuna/auth_service/controller/AuthController.java
@@ -1,4 +1,23 @@
 package com.ruhuna.auth_service.controller;
 
+import com.ruhuna.auth_service.service.AuthService;
+import com.ruhuna.auth_service.registerdto.*;
+import com.ruhuna.auth_service.logindto.*;
+import com.ruhuna.auth_service.responsedto.*;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.authentication.UserServiceBeanDefinitionParser;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    private final AuthService Authsevice;
+
+    public AuthController(AuthService Authservice){
+        this.Authsevice = Authservice;
+    }
 }
